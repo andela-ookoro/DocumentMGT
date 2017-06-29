4b107f91ad58ba793f596a383176b639471b1e8f
andela_ookoro <okwudiri.okoro@andela.com>
2017-06-29 15:35:24 +0100

    feat(document): edit document - create a component to edit document - create an action to send 'put' request to the server [Delivers #148076235]

--- client/actions/createDocument.js
+++ client/actions/createDocument.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import addJWT from './addJWT';
 
 export const createDocStatus = ( status, message ) => (
   {
@@ -8,20 +9,27 @@ export const createDocStatus = ( status, message ) => (
 );
 
 
-export const createDocument = (document) => {
-    const jwt = JSON.parse(localStorage.getItem('jwt'));
-    const config = {
-        headers: { 'Authorization':  jwt }
-    };
-   return axios.post('/documents', document, config)
-    .then(response => {
-      return createDocStatus('success', document.title);
-    })
-    .catch(error => {
-      if (error.response) {
-        return createDocStatus('failed', error.response.data.message);
-      }
-    });
+export const upsertDocument = (document, docId) => {
+  let url = '/documents';
+  let apiRequest;
+  // check docId is 0, then it is an insert; else it is an update
+  if (docId === 0) {
+    apiRequest = axios.post(url, document);
+  } else {
+    url = `${url}/${docId}`;
+    apiRequest = axios.put(url, document);
+  }
+  addJWT();
+
+  return apiRequest
+  .then(response => {
+    return createDocStatus('success', document.title);
+  })
+  .catch(error => {
+    if (error.response) {
+      return createDocStatus('failed', error.response.data.message);
+    }
+  });
 }
 
 
--- client/components/CreateDocument.js
+++ client/components/CreateDocument.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import tinymce from 'tinymce';
-import { createDocument } from '../actions/createDocument';
+import Parser from 'html-react-parser';
+import { upsertDocument } from '../actions/createDocument';
+import getDocument from '../actions/getDocument';
 
 class CreateDocument extends React.Component{
   constructor(props){
@@ -10,30 +12,79 @@ class CreateDocument extends React.Component{
     this.state = {
       message: '',
       title: '',
+      curDocument: {},
+      body: '',
+      docId: 0
     }
     this.saveDocument = this.saveDocument.bind(this);
     this.onChange = this.onChange.bind(this);
+    this.setDocument = this.setDocument.bind(this);
   }
 
   componentWillMount() {
-     tinymce.init({
-      selector: '#body',
-      height : 300
-    });
+    const documentID = this.props.match.params.documentId;
+    if (documentID) {
+      this.setDocument(getDocument(documentID));
+    }
   }
-  
 
+  /**
+   * set state to documents or error message
+   * @param {object} response - promise from the api called
+   * @memberof Documents
+   */
+  setDocument(apiResponse) {
+    // get server response and set state
+    apiResponse
+    .then(response => {
+      if (response.message) {
+        this.setState({
+          message: response.message
+        })
+        Materialize.toast(response.message, 3000, 'rounded');
+      } else {
+        const curDocument = response.document.data.data;
+        this.setState({
+          title: curDocument.title,
+          body: curDocument.body.toString(),
+          curDocument,
+          docId: curDocument.id
+        });
+
+        // find the selected access right
+        const accessRights = document.getElementsByName("accessRight");
+        const accessRight = curDocument.accessRight;
+
+        for(var i = 0; i < accessRights.length; i++) {
+          if(accessRights[i].id === accessRight) {
+            accessRights[i].checked = true;
+          }
+        }
+      }
+    })
+    .catch(err => {
+      console.log('err', err)
+    })
+  }
+
+  /**
+   * create a document object and send a request to create the document
+   * @param {*} e the submit button
+   */
   saveDocument(e) {
     e.preventDefault();
-    // get content of the text editor
-    const body =tinyMCE.get('body').getContent();
+    // get the  text editor content
+    const body =tinyMCE.get('content').getContent();
     // find the selected acess right
-    const accessRights = document.getElementsByName("accessMode");
+    const accessRights = document.getElementsByName("accessRight");
     let accessRight;
 
-    for(var i = 0; i < accessRights.length; i++) {
-      if(accessRights[i].checked)
-          accessRight = accessRights[i].id;
+    for(let i = 0; i < accessRights.length; i++) {
+      console.log(accessRights[i].id, accessRights[i].checked);
+      if(accessRights[i].checked) {
+        accessRight = accessRights[i].id;
+        break;
+      }
     }
 
     // create error message
@@ -42,7 +93,6 @@ class CreateDocument extends React.Component{
       message = "Please insert the content of the document";
     }  
     if (!accessRight) {
-      console.log('came here ')
       message += `${(message) ? ' and ' : 'Please'} 
         select an access mode`; 
     }
@@ -63,27 +113,37 @@ class CreateDocument extends React.Component{
         accessRight: accessRight,
         role: userInfo.role
       };
-      // call createDocument action 
-     createDocument(document)
+      console.log(document);
+      //call upsertDocument action 
+     upsertDocument(document, this.state.docId)
      .then(response => {
-       console.log('repsonse', response);
        if(response.status === 'success') {
         message = 'Document has been saved successfully';
+        // reset  state
+        this.setState({
+          message,
+          title: '',
+          curDocument: {},
+          body: ''
+        });
+        tinymce.get('content').setContent('');
       } else{
         message = response.message;
+        this.setState({
+          message
+        });
       }
-      this.setState({
-        message
-      });
      })
      .catch(err => {
        console.log('err', err);
      });
     }
-    
+    if (message) {
+      Materialize.toast(message, 3000, 'rounded')
+    }
   }
 
-    /**
+  /**
    * set the value of the control to the respective state node
    * @param {*} e 
    */
@@ -96,62 +156,61 @@ class CreateDocument extends React.Component{
  
 
   render(){
+    const body = this.state.body;
+     $(document).ready(() => {
+      tinymce.init({
+        selector: '#content',
+        height : 300,
+        width: '100%',
+        browser_spellcheck: true,
+      });
+      tinymce.get('content').on('init', (e) => {
+        e.target.setContent(body);
+      });
+    });
     return(
-        <div className="container">
-
-      <script>
-      </script>
+    <div className="container">
         <div className="body row">
-          <p> {this.state.message} </p>
+          <p id="message"> {this.state.message} </p>
           <form className="col s12" name="createDoc">
             <br />
-            <div className="row">
-              <div className="input-field col s12">
-                <i className="material-icons prefix">subject</i>
-                <input
-                  placeholder="Title" id="title" type="text" name="title"
-                  className="validate" value={this.state.title}
-                  onChange={this.onChange} required/>
-                <label htmlFor="title">Title</label>
-              </div>
+            <div className="input-field col s12">
+              <input
+                placeholder="Title" id="title" type="text" name="title"
+                className="validate" value={this.state.title}
+                onChange={this.onChange} autoFocus required/>
+              <label htmlFor="title">Title</label>
             </div>
-            <div className="row">
-              <div className="input-field col s12">
-                <i className="material-icons prefix">view_headline</i>
-                 <textarea
-                  placeholder="Body" id="body" type="text" name="body"
-                  className="materialize-textarea"
-                >
-                </textarea>
-                <label htmlFor="body">Body</label>
-              </div>
+            <div className="input-field col s12">
+                <textarea
+                placeholder="Body" id="content" type="text" name="body"
+                className="materialize-textarea"
+              >
+              </textarea>
             </div>
-            <div className="row">
-              <div className="input-field col l2 s12 m12">
-                Access Mode &nbsp;
-                <i className="material-icons prefix">lock</i>
-              </div>
-              <div className="input-field col l2 s4">
-                <input name="accessMode" type="radio" id="private" />
-                 <label htmlFor="private">Private</label>
-              </div>
-              <div className="input-field col l2 s4">
-                <input name="accessMode" type="radio" id="public" />
-                 <label htmlFor="public">Public</label>
-              </div>
-              <div className="input-field col l2 s4">
-                <input name="accessMode" type="radio" id="role" />
-                 <label htmlFor="role">Role</label>
-              </div>
+            <div className="input-field col l2 s12 m12">
+              Access Mode &nbsp;
+              <i className="material-icons prefix">lock</i>
             </div>
-            <div className="row">
-              <div className="input-field col s12" >
-                 <button
-                  className="btn waves-effect waves-light right" type="submit"
-                  name="action" onClick={this.saveDocument}>
-                   Submit <i className="material-icons right">send</i>
+            <div className="input-field col l2 s4">
+              <input name="accessRight" type="radio" id="private" />
+                <label htmlFor="private">Private</label>
+            </div>
+            <div className="input-field col l2 s4">
+              <input name="accessRight" type="radio" id="public" />
+                <label htmlFor="public">Public</label>
+            </div>
+            <div className="input-field col l2 s4">
+              <input name="accessRight" type="radio" id="role" />
+                <label htmlFor="role">Role</label>
+            </div>
+            <div className="input-field col s12" >
+              <button
+                className="btn waves-effect waves-light right" type="submit"
+                name="action" onClick={this.saveDocument}>
+                  {(body === '') ? 'Submit' : 'Update' } 
+                  <i className="material-icons right">send</i>
               </button>
-              </div>
             </div>
           </form>
         </div>
