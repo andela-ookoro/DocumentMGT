d1615690d8980bfef6ecb8803334cbedb29ac7ec
andela_ookoro <okwudiri.okoro@andela.com>
2017-07-13 10:22:32 +0100

    remove development only files

--- client/__tests__/e2e/timeout.js
+++
@@ -1,5 +0,0 @@
-import { defineSupportCode } from 'cucumber';
-
-defineSupportCode(({ setDefaultTimeout }) => {
-  setDefaultTimeout(80 * 1000);
-});
--- client/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import routes from './routes/index';
-
-ReactDOM.render(routes(), document.getElementById('app'));
